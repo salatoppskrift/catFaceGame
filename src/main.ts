013b3ba9f79f2383e6fca0be6665a349355cf58c
Martin R <martin.ristvedt@gmail.com>
2026-09-23 01:32:36 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,31 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+class Fifish extends HTMLElement {
+  private bbool = true;
+
+  constructor() {
+    super();
+  }
+  connectedCallback(): void {
+    this.render();
+  }
+  private render() {
+    this.innerHTML = /*HTML*/`
+      <button>
+        A fiiissssh ${this.bbool}
+      </button>
+    `;
+    this.addEventListener("click", () => {
+      this.bbool = !this.bbool;
+      this.render();
+    })
+  }
+}
+
+customElements.define("fi-fish", Fifish);
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+
+app.innerHTML = /*HTML*/`
+  <fi-fish></fi-fish>
 `;
